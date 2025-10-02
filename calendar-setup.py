b610b8339b4700f77287c9e62ef02f724e702ed9
Haoyan Li <haoyanli234@gmail.com>
2025-10-02 13:25:05 -0400

    Somehow got some generation working (incomplete), fixing week offset bugs. Currently behaves as if Monday is the first week, will be fixing

--- calendar-setup.py
+++ calendar-setup.py
@@ -1,13 +1,21 @@
 from pyscript import document
-import datetime
+from pyscript.web import *
+import datetime, calendar
+#no way there's literally an entire module dedicated to this *surprise*
 
+#Constants in caps
 MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
+#Document objects
 calendar_month = document.querySelector("#calendar-month")
+calendar_body = document.querySelector(".calendar-body")
 
-def setup():
-    calendar_month.innerText = MONTHS[int(datetime.date.today().month)-1]
-    #Oh no
-    
+#Very initial setup
+calendar.setfirstweekday(calendar.SUNDAY)
 
-setup()
+def setup(day,month,year):
+    calendar_month.innerText = str(MONTHS[month-1] + " " + str(year))    
+    for i in range(1,calendar.monthrange(year,month)[0]+1):
+        calendar_body.innerHTML += "<div class='calendar-day-past'>" + str(i) + "</div>"
+                                         
+
+setup(datetime.date.today().day, datetime.date.today().month, datetime.date.today().year)

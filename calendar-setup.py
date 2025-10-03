737987f37ace7c60332d5d81324df916d13a8921
Haoyan Li <haoyanli234@gmail.com>
2025-10-02 20:03:09 -0400

    Fixed offsets, generation, ids

--- calendar-setup.py
+++ calendar-setup.py
@@ -9,13 +9,19 @@ MONTHS = ["January", "February", "March", "April", "May", "June", "July", "Augus
 calendar_month = document.querySelector("#calendar-month")
 calendar_body = document.querySelector(".calendar-body")
 
-#Very initial setup
-calendar.setfirstweekday(calendar.SUNDAY)
-
 def setup(day,month,year):
     calendar_month.innerText = str(MONTHS[month-1] + " " + str(year))    
-    for i in range(1,calendar.monthrange(year,month)[0]+1):
+    index = 0
+    for i in range(1,(calendar.monthrange(year,month)[0]+2)%7):
         calendar_body.innerHTML += "<div class='calendar-day-past'>" + str(i) + "</div>"
+        index += 1
+    for i in range(1,calendar.monthrange(year,month)[1]+1):
+        calendar_body.innerHTML += "<div class='calendar-day' id=" + str(i).zfill(2) + "-" + str(month).zfill(2) + "-" + str(year) + ">" + str(i) + "</div>"
+        index += 1
+    while index % 7 != 0:
+        calendar_body.innerHTML += "<div class='calendar-day-past'>" + str(i) + "</div>"
+        index += 1
+
                                          
 
 setup(datetime.date.today().day, datetime.date.today().month, datetime.date.today().year)
